2a508f6c12899a1d794df64c549cc1108aebf1b4
WangU <alxeyuoouy@gmail.com>
2020-06-21 21:05:48 +0800

    Update httpclient.h

--- httpclient/httpclient.h
+++ httpclient/httpclient.h
@@ -28,7 +28,7 @@ struct addrinfo {
     int               ai_family;     /* Address family of socket. */
     int               ai_socktype;   /* Socket type. */
     int               ai_protocol;   /* Protocol of socket. */
-    socklen_t         ai_addrlen;    /* Length of socket address. */
+    uint32_t         ai_addrlen;    /* Length of socket address. */
     struct sockaddr  *ai_addr;       /* Socket address of socket. */
     char             *ai_canonname;  /* Canonical name of service location. */
     struct addrinfo  *ai_next;       /* Pointer to next in list. */
